bd13964612f175e7236db10951e7d0276f44d203
kondurusubbamma-2003 <kondurumaidhili@gmail.com>
2026-06-29 23:10:56 +0530

    Add files via upload

---
+++ problems.py
@@ -0,0 +1,37 @@
+#1
+# num=[1,2,3,4,5]
+# sum=0
+# for i in num:
+#     sum=sum+i*i
+# print("sum of squares:",sum)
+
+#2
+# def charcters(name):
+#    for i in name:
+#      print(i)
+# name="maidhili"
+# charcters(name)
+
+#3
+
+# num=[1,2,3,4,5]
+# max=0
+# for i in num:
+#   if i>max:
+#     max=i
+# print(max)
+
+#4
+
+# a = 0
+# b = 1
+
+# for i in range(10):
+#     print(a)
+#     a, b = b, a + b
+
+#5
+
+student={"name":"maidhili","age":22,"cource":"python"}
+for k,v in student.items():
+    print(k,"=",v)
